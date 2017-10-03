2214aa45ed93f6567718ac2bc300f4013057bb76
Tuan Dinh <dqtuan10@gmail.com>
2017-10-03 11:10:52 -0500

    cnn code

--- experiments/source/sbm_cnn.py
+++ experiments/source/sbm_cnn.py
@@ -12,6 +12,7 @@ class GGAN:
         self.dim_z = dim_z # Noise size
         self.dim_x = dim_x # Real input Size
         self.dim_h = dim_h # hidden layers
+        self.N = int(np.sqrt(self.dim_x))
 
         # Generator network params
         self.Z = tf.placeholder(tf.float32, shape=[None, self.dim_z])
@@ -48,6 +49,48 @@ class GGAN:
         in_dim = size[0]
         xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
         return tf.random_normal(shape=size, stddev=xavier_stddev)
+
+    def cnn_model_fn(self, features, labels, mode):
+      """Model function for CNN."""
+      # Input Layer
+      input_layer = tf.reshape(features["x"], [-1, self.N, self.N, 1])
+
+      # Convolutional Layer #1
+      conv1 = tf.layers.conv2d(
+          inputs=input_layer,
+          filters=32,
+          kernel_size=[5, 5],
+          padding="same",
+          activation=tf.nn.relu)
+
+      # Pooling Layer #1
+      pool1 = tf.layers.max_pooling2d(inputs=conv1,
+                    pool_size=[2, 2], strides=2)
+
+      # Convolutional Layer #2 and Pooling Layer #2
+      conv2 = tf.layers.conv2d(
+          inputs=pool1,
+          filters=64,
+          kernel_size=[5, 5],
+          padding="same",
+          activation=tf.nn.relu)
+      pool2 = tf.layers.max_pooling2d(inputs=conv2,
+                        pool_size=[2, 2], strides=2)
+
+      # Dense Layer
+      final_dim = int(self.N/4)
+      pool2_flat = tf.reshape(pool2, [-1, final_dim * final_dim * 64])
+      dense = tf.layers.dense(inputs=pool2_flat,
+                    units=1024, activation=tf.nn.relu)
+      dropout = tf.layers.dropout(
+          inputs=dense, rate=0.4,
+            training=mode == tf.estimator.ModeKeys.TRAIN)
+
+      # Logits Layer
+      logits = tf.layers.dense(inputs=dropout, units=1)
+
+      return logits
+
 
     def sample_Z(self, m, n):
         return np.random.uniform(-1., 1., size=[m, n])
@@ -192,8 +235,8 @@ def generate_sbm(N, m):
     return graphs
 
 if __name__ == '__main__':
-    N = 90
-    dim_z = 500
+    N = 40
+    dim_z = 100
     dim_x = N * N
     dim_h = 512
     dataset_size = 10000
